d606b115d900617d61449446f51b90796137a6e5
jerryt92 <jerrytian92@outlook.com>
2025-08-12 11:42:30 +0800

    fix(restUtils): 修复非 JSON 响应导致的解析错误- 在打印响应体之前，增加对 Content-Type 的检查 -防止因非 JSON 响应导致的解析错误

--- utills/restUtils.py
+++ utills/restUtils.py
@@ -18,7 +18,7 @@ def printResponse(response: requests.Response, pretty=True):
     print('Status Code: %s' % response.status_code)
     headersDict = dict(response.headers)
     print('Headers:', json.dumps(headersDict, indent=2 if pretty else None))
-    if 'application/json' in headersDict['Content-Type']:
+    if 'Content-Type' in headersDict and 'application/json' in headersDict['Content-Type']:
         print('Body:', json.dumps(response.json(), indent=2 if pretty else None))
     else:
         print('Body:', response.text)
